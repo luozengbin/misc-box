37958087f6465874c9fff9ddcff6e185daa7e683
luozengbin <jalen.cn@gmail.com>
2015-07-03 00:01:59 +0900

    update jvm stack shell

--- jvm_stacksize.sh
+++ jvm_stacksize.sh
@@ -1,14 +1,40 @@
 #!/bin/sh
+###########################################################################
+# jvm_stacksize.sh - take jvm stack size snapshot
+#
+#    Authors: Akira Wakana <jalen.cn@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Usage:
+#    $jvm_stacksize.sh <JVM ProcessID>
+###########################################################################
 
-if [ ! $# -eq 1 ]; then
-    echo "Usage ${0} <JVM PID>"
-    exit 1
-fi
 
-printf "[ PID ]\t[StackSize]\t[GuardPages]\t[Thread Name]\n"
+PID=$1
+
+ps ${PID} | grep [j]ava > /dev/null || {
+    echo "-----------------------------------------------------------------------"
+    ps -ef  | grep [j]ava
+    echo "-----------------------------------------------------------------------"
+    echo -n "please input the java process id: "
+    read PID
+}
+
+printf "[ PID ]\t[StackSize(kB)]\t[GuardPage(kB)]\t[UsedSize(kB)]\t[Thread Name]\n"
 
 # jstackの出力結果からスレッドIDと名前を抽出する
-jstack $1 | grep nid | sed -e "s/^\"\(.*\)\".*nid=\(0x[0-9|a-z]*\).*$/\2,\1/" | sort | while read line
+jstack ${PID} | grep nid | sed -e "s/^\"\(.*\)\".*nid=\(0x[0-9|a-z]*\).*$/\2,\1/" | sort | while read line
 do
     # スレッドIDを切り出す
     pid_hex=`echo "${line}" | awk -F"," '{print $1}'`
@@ -20,8 +46,9 @@ do
     pid=`printf '%d\n' ${pid_hex}`
 
     # /proc/<pid>/smaps ファイルからスタックサイズ、ガードページサイズを取得する
-    guard_page=`cat /proc/$1/smaps | grep -B15 "stack:${pid}"| head -1 | awk '{print $2}'`
-    stack_page=`cat /proc/$1/smaps | grep -A1 "stack:${pid}" | tail -1 | awk '{print $2}'`
+    guard_page=`cat /proc/${PID}/smaps | grep -B16 "stack:${pid}" | grep -e "^Size:" | awk '{print $2}'`
+    stack_page=`cat /proc/${PID}/smaps | grep -A15 "stack:${pid}" | grep -e "^Size:" | awk '{print $2}'`
+    used_size=`cat /proc/${PID}/smaps  | grep -A15 "stack:${pid}" | grep -e "^Rss:" | awk '{print $2}'`
     stack_size=`expr ${guard_page} + ${stack_page}`
-    printf "%7d\t%11s\t%12s\t%s\n" "${pid}" "${stack_size}Kb" "${guard_page}Kb" "${thread_name}"
+    printf "%7d\t%15s\t%15s\t%14s\t%s\n" "${pid}" "${stack_size}" "${guard_page}" "${used_size}" "${thread_name}"
 done
